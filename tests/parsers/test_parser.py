db6fe3398ef4830f471e57d1e8ea129a6a24009c
GarzonDiegoFEUP <up202010843@up.pt>
2024-10-11 16:53:48 +0100

    add requirements

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,4 +1,4 @@
-import logging
+#import logging
 
 from nomad.datamodel import EntryArchive
 
@@ -7,8 +7,8 @@ from nomad_inl_base.parsers.parser import NewParser
 
 def test_parse_file():
     assert True
-    #parser = NewParser()
-    #archive = EntryArchive()
+    parser = NewParser()
+    archive = EntryArchive()
     #parser.parse('tests/data/example.out', archive, logging.getLogger())
 
     #assert archive.workflow2.name == 'test'
